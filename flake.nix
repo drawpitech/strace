8c92c4733cd1c43dab42f00c568ffcb7d2ab47d3
Clément <clement2104.boillot@gmail.com>
2024-03-30 17:14:48 +0100

    ci: remove unittest
    who needs unit testing when the code compiles?

--- flake.nix
+++ flake.nix
@@ -4,29 +4,50 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
         pkgs = nixpkgs.legacyPackages.${system};
         cc = pkgs.gcc12;
-      in
+      in rec
       {
         devShells.default = pkgs.mkShell {
-          packages = [ cc ] ++ (with pkgs; [
-            glibc
-            gnumake
-            hexyl
-            criterion
-            gcovr
-            ltrace
-            strace
-            valgrind
-            bear
-            gdb
-            nasm
-          ]);
+          packages =
+            packages.strace.buildInputs
+            ++ (with pkgs; [
+              hexyl
+              ltrace
+              strace
+              valgrind
+              bear
+              gdb
+            ]);
         };
 
-        formatter = pkgs.nixpkgs-fmt;
+        formatter = pkgs.alejandra;
+
+        packages = {
+          default = packages.strace;
+          strace = pkgs.stdenv.mkDerivation rec {
+            name = "my_strace";
+            src = ./.;
+
+            buildInputs = [cc] ++ (with pkgs; [glibc gnumake]);
+            makeFlags = ["CC=${cc}/bin/gcc"];
+            hardeningDisable = ["format" "fortify"];
+            enableParallelBuilding = true;
+
+            buildPhase = ''
+              make ${name}
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ${name} $out/bin
+            '';
+          };
+        };
       });
 }
